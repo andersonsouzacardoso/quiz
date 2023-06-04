88b2439d07016873ac2e1feec1d880587b78969b
andersonsouzacardoso <andersonfdl@gmail.com>
2023-06-04 13:09:43 -0300

    quiz commit

---
+++ quiz.js
@@ -0,0 +1,350 @@
+let pergunta = [
+{
+    perguntas: 'Qual o significado da sigla HTML?',
+    alternativas: ['Hiper Text Markup Language','Hiperlinks and Text Markup Language','Home Tool Markup Language','Cascating Style Sheets'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual a tag que determina o principal assundo abordado em uma pagina?',
+    alternativas: ['<h6>','<head>','<strong>','<h1>'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual é o elemento HTML correto para inserir uma quebra de linha?',
+    alternativas: ['<break>','<br>','<pre>','<div>'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual a tag para criar uma lista NÃO ORDENADA?',
+    alternativas: ['<ol>','<li>','<ul>','<h1>'],
+    correta: 2
+},
+
+{
+    perguntas: 'Os comandos <strong>, <em> e <U> significam sucessivamente:',
+    alternativas: ['Itálico, Negrito e Sublinhado','Sublinhado, Negrito e Itálico','Negrito, Sublinhado e Itálico','Negrito, Itálico e Sublinhado'],
+    correta: 3
+},
+
+{
+    perguntas: 'Para criar tabelas HTML, você ultiliza quais comandos?',
+    alternativas: ['<TABLE>, <TR> e <TD>','<TABLE>, <BODY> e <TD>','<TABLE>, <TH> e <P>','<TABLE>, <UL> e <TR>'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual a tag para criar o corpo de uma página HTML?',
+    alternativas: ['<TITLE>','<BODY>','<HEAD>','<DIV>'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual a tag representa um trecho de destaque em um texto? (Marca texto).',
+    alternativas: ['<p>','<h3>','<strong>','<mark>'],
+    correta: 3
+},
+
+{
+    perguntas: 'Em uma lista não ordenada existe 3 tipos de marcadores, qual o marcador que representa um quadrado?',
+    alternativas: ['<UL TYPE="square">','<UL TYPE="disc">','<UL TYPE="circle">','<UL TYPE="quadrado">'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual a tag para criar uma lista ORDENADA?',
+    alternativas: ['<LI>','<OL>','<UL>','<DT>'],
+    correta: 1
+},
+{
+    perguntas: 'Em uma lista ordenada tem como padrão lista numérica, para o formato números romanos maiúsculo qual a tag?',
+    alternativas: ['<OL TYPE="A" START="1">','<OL TYPE="1" START="1">','<OL TYPE="I" START="1">','<OL TYPE="a" START="1">'],
+    correta: 2
+},
+
+{
+    perguntas: 'Qual a tag para criar uma lista DESCRITIVA?',
+    alternativas: ['<DL>','<DT>','<DD>','<UL>'],
+    correta: 0
+},
+
+{
+    perguntas: 'Dentro da lista descritiva, qual tag é usada para descrição da lista?',
+    alternativas: ['<UL>','<DL>','<DT>','<DD>'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual a tag é usada para criar um link?',
+    alternativas: ['<title>','<a HREF="">','<head>','<html>'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual é a tag usada para fechar um link?',
+    alternativas: ['</body>','</div>','</a>','</p>'],
+    correta: 2
+},
+
+{
+    perguntas: 'Para criar uma ANCORA ou mais dentro de uma página HTML usamos a tag?',
+    alternativas: ['<a href="#1"</a>','<link rel="stylesheet" href"">','<script src=""></script>','<www.google.com>'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual a tag é usada para inserir uma imagem no HTML?',
+    alternativas: ['<div>','<a HREF="">','<body>','<img src"">'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual a tag é usada para criar uma TABELA no HTML?',
+    alternativas: ['<html>','<table>','<head>','<h1>'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual a tag define as linhas de uma tabela?',
+    alternativas: ['<td>','<th>','<tr>','<table>'],
+    correta: 2
+},
+
+{
+    perguntas: 'Qual a tag define o cabeçalho de uma tabela?',
+    alternativas: ['<table>','<td>','<tr>','<th>'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual a tag define os dados de uma tabela?',
+    alternativas: ['<td>','<tr>','<table>','<th>'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual a tag usamos para iniciar um FORMULÁRIO?',
+    alternativas: ['<html></html>','<form></form>','<div></div>','<head></head>'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual INPUT usamos para criar um campo de senha? <input type="???">',
+    alternativas: ['<input type="radio">','<input type="submit">','<input type="password">','<input type="text">'],
+    correta: 2
+},
+
+{
+    perguntas: 'Qual INPUT usamos para criar uma caixa de seleção? <input type="???">',
+    alternativas: ['<input type="textarea">','<input type="imagem">','<input type="radio">','<input type="checkbox">'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual INPUT usamos para criar uma área de texto? <input type="???">',
+    alternativas: ['<input type="text">','<input type="date">','<input type="reset">','<input type="submit">'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual INPUT usamos para limpar os campos de um formulário? <input type="???">',
+    alternativas: ['<input type="password">','<input type="reset">','<input type="color">','<input type="number">'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual é a tag usada para criar um rodapé na pagina html?>',
+    alternativas: ['<select>','<head>','<footer>','<body>'],
+    correta: 2
+},
+
+{
+    perguntas: 'O que significa CSS?',
+    alternativas: ['Cascading Style Sheets','Computer Style Sheets','Colorfull Style Sheets','Creative Style Sheets'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual tag usamos para se referir a uma folha de estilo externa?',
+    alternativas: ['<StyleSheet>mystyle.css</StyleSheet>','<style.src="mystyle.css">','<script src="style"></script>','<link rel="stylesheet" href="style.css">'],
+    correta: 3
+},
+
+{
+    perguntas: 'Onde em um documento HTML é o lugar correto para se referir a uma folha de estilo externa?',
+    alternativas: ['Na sessão <body>','Na sessão <head>','Na sessão <footer>','Em uma <div>'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual tag HTML é usada para definir uma folha de estilo interna?',
+    alternativas: ['<html>','<css>','<style>','<script>'],
+    correta: 2
+},
+
+{
+    perguntas: 'Qual é a sintaxe CSS correta?',
+    alternativas: ['body{color: black;}','{body color: black;}','body color: black;','{body; color: black;}'],
+    correta: 0
+},
+
+{
+    perguntas: 'Como você insere um comentário em um arquivo CSS?',
+    alternativas: ['"this is a comment"','//*this is a comment.','-->this is a comment<--','/*this is a comment*/'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual propriedade é usada para alterar a cor de fundo?',
+    alternativas: ['color','background-color','bgcolor','backgroud-url'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual propriedade CSS é usada para alterar a cor do texto de um elemento?',
+    alternativas: ['text-color','background-color','color','fgcolor'],
+    correta: 2
+},
+
+{
+    perguntas: 'Qual propriedade CSS controla o tamanho do texto?',
+    alternativas: ['font-size','text-size','font-style','text-style'],
+    correta: 0
+},
+
+{
+    perguntas: 'Qual é a sintaxe CSS correta para tornar todos os elementos <p> em negrito?',
+    alternativas: ['p{text-size: bold;}','<p style="text-size: bold;">','<p style="font-size: bold;">','p{font-weight: bold;}'],
+    correta: 3
+},
+
+{
+    perguntas: 'Como você exibe hiperlinks sem sublinhado?',
+    alternativas: ['a{underline: none;}','a{text-decoration: none;}','a{decoration:no-underline;}','a{text-decoration:no-underline;}'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual propriedade é usada para alterar a margem esquerda de um elemento?',
+    alternativas: ['margin-left','padding-left','ident','align-center'],
+    correta: 0
+},
+
+{
+    perguntas: 'Como você seleciona um elemento com id "demo"?',
+    alternativas: ['=>demo','.demo','#demo','*demo'],
+    correta: 2
+},
+
+{
+    perguntas: 'Como você seleciona elementos com o nome de classe "test"?',
+    alternativas: ['*teste','#teste','teste','.teste'],
+    correta: 3
+},
+
+{
+    perguntas: 'Como você seleciona todos os elementos p dentro de um elemento div?',
+    alternativas: ['div+p','div.p','div=p','div p'],
+    correta: 1
+},
+
+{
+    perguntas: 'Qual é o valor padrão da propriedade position?',
+    alternativas: ['fixed','absolite','static','relative'],
+    correta: 2
+},
+
+{
+    perguntas: 'Como centralizar um texto em CSS?',
+    alternativas: ['text-align: top;','text-align: last;','text-align: bottom;','text-align: center;'],
+    correta: 3
+},
+
+{
+    perguntas: 'Qual propriedade é usada para estabelecer valor minino e maximo para uma altura?',
+    alternativas: ['height','padding','float','margin-top'],
+    correta: 0
+},
+]
+
+let aplicativo = {
+ start: function(){
+    this.perguntaAtual = 0;
+    this.totalPontos = 0;
+
+    let minhasAlternativas = document.querySelectorAll('.alternativa');
+    minhasAlternativas.forEach((element,index)=>{
+        element.addEventListener('click', ()=>{
+           this.checaResposta(index);
+        })
+    })
+
+    this.atualizaPontos();
+    aplicativo. mostraquestao(pergunta[this.perguntaAtual]);
+},
+mostraquestao: function(q){
+    this.questaoAtual = q;
+    //Mostrando perguntas
+    let titleDiv = document.getElementById('perguntas');
+    titleDiv.textContent = q.perguntas;
+    //Mostrando Alternativas
+    let minhasAlternativas = document.querySelectorAll('.alternativa');
+    minhasAlternativas.forEach(function(element,index){
+        element.textContent = q.alternativas[index];
+        
+    })
+},
+
+    proximaPergunta: function(){
+        this.perguntaAtual++;
+        if(this.perguntaAtual == pergunta.length){
+            this.perguntaAtual = 0;
+        }
+    },
+
+    checaResposta: function(user){
+
+        if(this.questaoAtual.correta == user){
+            console.log('ta certo')
+            this.totalPontos++;
+            this.mostraResposta(true);
+            document.getElementById('resultado').style.backgroundColor = 'green';
+        }else{
+            console.log('ta não')
+            this.mostraResposta(false);
+            document.getElementById('resultado').style.backgroundColor = 'red';
+        }
+        this.atualizaPontos();
+        this.proximaPergunta();
+        this.mostraquestao(pergunta[this.perguntaAtual])
+    },
+
+    atualizaPontos: function(){
+        let scoreDiv = document.getElementById('pontos')
+        scoreDiv.textContent = `Sua pontuação é: ${this.totalPontos}`
+        
+    },
+
+    mostraResposta: function(correta){
+        let resultDiv = document.getElementById('resultado')
+        let result = ''
+        //Formatar como a mensagem será exibida
+        if(correta){
+            result = 'Resposta Correta'
+        }else{
+            //Obtendo a questão atual
+            let perguntas = pergunta[this.perguntaAtual]
+            //Obtenha o indice da resposta correta da questão atual
+            let indiceRespostacorreta = perguntas.correta;
+            //Obter o texto correto da questão atual
+            let textoCorreto = perguntas.alternativas[indiceRespostacorreta]
+            result = `Incorreto! Resposta Correta: ${textoCorreto}`
+        }
+
+        resultDiv.textContent = result
+    }
+
+}
+aplicativo. start();
